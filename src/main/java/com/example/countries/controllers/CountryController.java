325017576f9d4b60552c1d56b668cc7003ac908a
albertsimonsen <alsi0001@stud.kea.dk>
2025-10-06 09:41:12 +0200

    comitted database config

---
+++ src/main/java/com/example/countries/controllers/CountryController.java
@@ -0,0 +1,26 @@
+package com.example.countries.controllers;
+
+import com.example.countries.models.Country;
+import com.example.countries.services.CountryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+}
